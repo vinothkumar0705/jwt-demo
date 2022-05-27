9dd4438015685f5d32554fff37a0ad2ab39330fd
vinot <vinothkumar1.selvaraj@outlook.com>
2022-05-27 08:07:15 +0530

    prefilter and postfilter

---
+++ apigateway-service/src/main/java/com/demo/apigateway/MyPreFilter.java
@@ -0,0 +1,35 @@
+package com.demo.apigateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Set;
+
+@Component
+public class MyPreFilter implements GlobalFilter {
+
+    final Logger logger = LoggerFactory.getLogger(MyPreFilter.class);
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("My first pre-filter executed...............");
+
+        String requestPath = exchange.getRequest().getPath().toString();
+        logger.info("Request Path = "+requestPath);
+
+        HttpHeaders headers = exchange.getRequest().getHeaders();
+
+        Set<String> headerNames = headers.keySet();
+
+        headerNames.forEach((headerName)->{
+            String headerValue = headers.getFirst(headerName);
+            logger.info(headerName +" "+ headerValue);
+        });
+        return chain.filter(exchange);
+    }
+}
